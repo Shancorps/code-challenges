27ecb3a89121af7b63996ca9c364785f0f722a90
SamLewis347 <samuellewis92@icloud.com>
2025-02-08 15:38:50 -0500

    added email validation

--- python-challenges/data-processor/data_processor_shell.py
+++ python-challenges/data-processor/data_processor_shell.py
@@ -14,8 +14,68 @@ class DataValidator:
     @staticmethod
     def validate_email(email: str) -> bool:
         """Validate email format."""
-        # TODO: Implement email validation
-        pass
+        #checking for the existance of exactly ONE "@" symbol
+        if email.count('@') != 1:
+            return False
+        
+        #splitting the email into the local (pre @) and domain (post @) parts
+        local_string, domain_string = email.split('@')
+
+        #making sure that the local_string and the domain_string are not empty
+        if not local_string or not domain_string:
+            return False
+        
+        #making sure that the local_string is within the 64 character length limit
+        if len(local_string) > 64:
+            return False
+        
+        #making sure that the domain_string is within the 253 character length limit
+        if len(domain_string) > 253:
+            return False
+        
+        #makeing sure that the local_string does not start or end with a period
+        if local_string[0] == '.' or local_string[-1] == '.':
+            return False
+        
+        #making sure that the local_string doesn't contain '..'
+        if '..' in local_string:
+            return False
+        
+        #making sure the domain_string does not start or end with a hyphen
+        if domain_string[0] == '.' or domain_string[-1] == '-':
+            return False
+        
+        #making sure the domain_string contains a period seperating the domain and TLD
+        if '.' not in domain_string:
+            return False
+        
+        #splitting the domain_string into the domain and the TLD
+        domain_parts = domain_string.split('.')
+
+        #making sure the TLD is at least 2 characters long
+        if len(domain_parts[-1]) < 2:
+            return False
+        
+        #making sure the domain does not contain double periods
+        if '..' in domain_string:
+            return False
+        
+        #checking if the local_string and domain_string contain only valid characters
+        valid_local_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._%+-"
+        valid_domain_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.-"
+
+        for c in local_string:
+            if c not in valid_local_chars:
+                return False
+
+        # Check domain part characters
+        for c in domain_string:
+            if c not in valid_domain_chars:
+                return False
+        
+        #if the email passes all of these tests then it is a valid email
+        return True
+
 
     @staticmethod
     def validate_age(age: int) -> bool:
